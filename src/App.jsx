1835c34f30ac19d5a3b6a1590519858592ff1081
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2024-07-11 10:20:38 +0100

    done with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import HomeBody from './components/HomeComponent/HomeBody';
 import Navbar from './components/Navbar';
@@ -8,6 +9,7 @@ function App() {
 			<Navbar />
 			<Hero />
 			<HomeBody />
+			{<Footer />}
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,71 @@
+import { FaFacebook } from 'react-icons/fa';
+import { RiTwitterXFill } from 'react-icons/ri';
+import { FaSquareInstagram } from 'react-icons/fa6';
+import { FaYoutube } from 'react-icons/fa';
+
+const Footer = () => {
+	return (
+		<div className='mt-10 bg-lightLemon h-80'>
+			<div className='flex my-10 mx-4 h-80 '>
+				<div className='w-1/4 h-full  flex flex-col justify-center'>
+					<div className=' w-16 border-2 border-dashed border-normalGreen flex flex-col justify-center items-center'>
+						<h1 className='text-[14px]'>Shapel</h1>
+						<p className=' text-[10px]'>Beauty Shop</p>
+					</div>
+
+					<div className='mt-6'>
+						<p>Subscribe to our mailing list to</p>
+						<p>get the new updates</p>
+					</div>
+
+					<div className='mt-10 flex gap-5'>
+						<FaFacebook className='text-blue-900' />
+						<RiTwitterXFill />
+						<FaSquareInstagram className='text-pink-700' />
+						<FaYoutube className='text-red-800' />
+					</div>
+				</div>
+
+				<div className='w-1/4 h-full flex flex-col justify-center '>
+					<p className='font-bold text-xl mb-6'>INFORMATION</p>
+
+					<div className='flex flex-col gap-4'>
+						<p>About Us</p>
+
+						<p>Contact</p>
+
+						<p>How we source</p>
+
+						<p>Identifying False Products</p>
+					</div>
+				</div>
+
+				<div className='w-1/4 h-full flex flex-col justify-center '>
+					<p className='font-bold text-xl mb-6'>OUR SERVICE</p>
+
+					<div className='flex flex-col gap-4'>
+						<p>Shipping & Delivery</p>
+
+						<p>Refunds & Returns</p>
+
+						<p>Book A Consultation</p>
+					</div>
+				</div>
+
+				<div className='w-1/4 h-full flex flex-col justify-center '>
+					<p className='font-bold text-xl mb-6'>MY ACCOUNT</p>
+
+					<div className='flex flex-col gap-4'>
+						<p>My Account</p>
+
+						<p>My Cart</p>
+
+						<p>Checkout</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
