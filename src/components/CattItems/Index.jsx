17141017792ea175167cec31ccc575e8145bd2d9
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2024-07-12 04:10:04 +0100

    ok

--- src/components/CattItems/Index.jsx
+++ src/components/CattItems/Index.jsx
@@ -1,38 +1,73 @@
 import React from 'react';
+import cart1 from '../../assets/cart1.png';
+import { FaArrowRightLong } from 'react-icons/fa6';
 
 const Index = () => {
 	return (
-		<div className=''>
+		<div className='m-4 md:m-16'>
 			<div>
-				<h1>Cart</h1>
+				<h1 className='text-4xl font-bold'>Cart</h1>
 			</div>
 
-			<table className='table-auto mt-10'>
+			<table className='table-auto mt-10 w-full'>
 				<thead>
-					<tr>
-						<th>Song</th>
-						<th>Artist</th>
-						<th>Year</th>
+					<tr className='text-left bg-lightGray h-16'>
+						<th>PRODUCT</th>
+						<th>PRICE</th>
+						<th>QUANTITY</th>
+						<th>SUBTOTAL</th>
 					</tr>
 				</thead>
-				<tbody>
-					<tr>
-						<td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-						<td>Malcolm Lockyer</td>
-						<td>1961</td>
+				<tbody className='mt-8 '>
+					<tr className='h-16'>
+						<td className='flex items-center'>
+							{' '}
+							<img
+								src={cart1}
+								alt='shop1'
+							/>
+							<p>Olay Vitamin C Brightening Body Lotion 502ML</p>
+						</td>
+						<td>₦33,000</td>
+						<td className=''>
+							<span className='font-bold'> -</span> 1{' '}
+							<span className='font-bold'> +</span>
+						</td>
+						<td>₦33,000</td>
 					</tr>
-					<tr>
-						<td>Witchy Woman</td>
-						<td>The Eagles</td>
-						<td>1972</td>
-					</tr>
-					<tr>
-						<td>Shining Star</td>
-						<td>Earth, Wind, and Fire</td>
-						<td>1975</td>
+
+					<tr className='h-16 mt-8 border-y-2 border-black'>
+						<td className='flex items-center'>
+							{' '}
+							<img
+								src={cart1}
+								alt='shop1'
+							/>
+							<p>Olay Vitamin C Brightening Body Lotion 502ML</p>
+						</td>
+						<td>₦33,000</td>
+						<td className=''>
+							<span className='font-bold'> -</span> 1{' '}
+							<span className='font-bold'> +</span>
+						</td>
+						<td>₦33,000</td>
 					</tr>
 				</tbody>
 			</table>
+
+			<div className='w-full mt-10 flex flex-col justify-end items-end'>
+				<div className='flex justify-between w-1/3 h-10 items-center bg-lightGray'>
+					<p className='font-semibold pl-4'>TOTAL</p>
+					<p className='font-semibold pr-4'>₦99,000</p>
+				</div>
+			</div>
+
+			<div className='w-full mt-1 flex flex-col justify-end items-end'>
+				<div className=' mt-5 flex gap-3 justify-center w-1/4 h-10 items-center bg-lightGreen'>
+					<p className='font-semibold pl-4'>PROCEED TO CHECKOUT</p>
+					<FaArrowRightLong />
+				</div>
+			</div>
 		</div>
 	);
 };
